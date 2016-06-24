399cdc9cd574a7653b3db578bc04180167da71e9
Leandro Ostera <leandro@ostera.io>
2016-06-25 01:50:12 +0200

    Declaratively bind to some events

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 //@flow
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-const ControlRoomApp = React.createClass({
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/observable/dom/webSocket'
+import 'rxjs/add/observable/from'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/filter'
 
-})
+const URI = "ws://localhost:8080/websocket"
 
-React.render( (
-  <ControlRoomApp />
-), document.getElementById('controlrooom'))
+let socket = Observable.webSocket(URI)
+
+let save = (event) => {
+  localStorage.setItem("controlroom.last_event", JSON.stringify(event))
+  localStorage.setItem("controlroom.events", JSON.stringify(events))
+}
+
+Observable.from(socket).do(save).subscribe()
+
+let bind = (event, callback) => {
+  Observable
+    .from(socket)
+    .filter( e => e.event === event )
+    .subscribe(callback)
+}
+
+bind("queue_push", console.log.bind(console, "QUEUE_PUSH:"))
+bind("queue_consumer_stopped", console.log.bind(console, "QUEUE_CONSUMER_STOPPED:"))
