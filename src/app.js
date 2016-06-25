47f23de61b6992cd8bbbeb4bd8c4bc6452fde6b8
Leandro Ostera <leandro@ostera.io>
2016-06-25 12:32:10 +0200

    Buffer incoming events

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 //@flow
 
 /*******************************************************************************
+ * Utils!
+ *******************************************************************************/
+
+const log = console.log.bind(console)
+
+let __uuid_counter=0
+let uuid = (key) => {
+  __uuid_counter += 1
+  return __uuid_counter
+}
+
+/*******************************************************************************
  * Presentation!
  *******************************************************************************/
 
@@ -23,21 +35,15 @@ const NavBar = React.createClass({
 const Event = React.createClass({
   render() {
     let e = this.props.event
-    return (
-      <section>
-        <h4>{e.event}</h4>
-      </section>
-    )
+    return ( <li> {e._key} - {e.event } </li>)
   }
 })
 
 const Events = React.createClass({
   render() {
-    console.log(this.props)
-    // this.props.events.events._data[]
-    let events = this.props.events.map( e => {
-      (<li> <Event event={e} /> </li>)
-    })
+    let events = this.props.events
+      .reverse()
+      .map( e => (<Event key={e._key} event={e} />) )
 
     return (
       <ul>
@@ -49,13 +55,10 @@ const Events = React.createClass({
 
 const ControlRoomApp = React.createClass({
   render() {
-    console.log(this.props)
-    // this.props.events._data[]
-    let events = this.props.events
     return (
       <section>
         <NavBar />
-        <Events events={{events}} />
+        <Events {...this.props} />
       </section>
     )
   }
@@ -68,39 +71,31 @@ const ControlRoomApp = React.createClass({
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/observable/dom/webSocket'
 import 'rxjs/add/observable/from'
+import 'rxjs/add/operator/bufferCount'
 import 'rxjs/add/operator/do'
-import 'rxjs/add/operator/filter'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/scan'
 
 const URI = "ws://localhost:8080/websocket"
 
 let socket = Observable.webSocket(URI)
 
-let _data = []
-let save = (event) => {
-  _data.push(event)
-  localStorage.setItem("controlroom.last_event", JSON.stringify(event))
-}
+let addUUID = (e) => Object.assign(e, {
+  _key: uuid('event')
+})
 
-let render = (event) => {
+let render = (events) => {
   ReactDOM.render( (
-    <ControlRoomApp events={{_data}} />
+    <ControlRoomApp events={events} />
   ), document.getElementById("controlroom"))
 }
 
 Observable
   .from(socket)
-  .do(save)
+  .map(addUUID)
+  .bufferCount(5)
+  .scan( (acc, events) => acc.concat(events) )
+  .do(log)
   .subscribe(render)
 
-
-/*
-let bind = (event, callback) => {
-  Observable
-    .from(socket)
-    .filter( e => e.event === event )
-    .subscribe(callback)
-}
-
-bind("event_push", console.log.bind(console, "event_PUSH:"))
-bind("event_consumer_stopped", console.log.bind(console, "event_CONSUMER_STOPPED:"))
-*/
+render([])
